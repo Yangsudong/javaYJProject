58b2ed83379a87b0fe591aa2eed612f7b870352e
admin <tnehdtlrl@naver.com>
2020-06-05 16:59:05 +0900

    0605

---
+++ src/javaYJProject/exchangeRateController.java
@@ -0,0 +1,19 @@
+package javaYJProject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+public class exchangeRateController implements Initializable{
+	@FXML ComboBox comboPublic1,comboPublic2;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+
+}
