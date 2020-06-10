244a251980a0a1ed95d326470457f9709a1120ee
jininseok <64175749+jininseok@users.noreply.github.com>
2020-06-10 12:28:47 +0900

    Update Hcontroller.java

--- src/javaYJProject/Hcontroller.java
+++ src/javaYJProject/Hcontroller.java
@@ -1,5 +1,45 @@
 package javaYJProject;
+package javaYJProject;
 
-public class Hcontroller {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class Hcontroller implements Initializable{
+@FXML AnchorPane holderPane;
+	AnchorPane home;
+	@FXML Button btnH,btnK,btnB,btnBW,btnA;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		createPage();
+	}
+	
+	private void setNode(Node node) {
+		holderPane.getChildren().clear();
+		holderPane.getChildren().add((Node) node);
+		
+		FadeTransition ft = new FadeTransition();
+		ft.setNode(node);
+		ft.setFromValue(0.1);
+		ft.setToValue(1);
+		ft.setCycleCount(1);
+		ft.setAutoReverse(false);
+		ft.play();
+	}
+	private void createPage() {
+		try {
+			home = FXMLLoader.load(getClass().getResource("/FXML/home.fxml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
