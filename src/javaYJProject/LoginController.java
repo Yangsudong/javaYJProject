8273e6a26dd88e4cb282b5274842d3b4251bda9b
jininseok <64175749+jininseok@users.noreply.github.com>
2020-06-10 12:29:37 +0900

    Update LoginController.java

--- src/javaYJProject/LoginController.java
+++ src/javaYJProject/LoginController.java
@@ -1,5 +1,56 @@
 package javaYJProject;
 
-public class LoginController {
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.util.ResourceBundle;
 
+import javafx.animation.PauseTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class LoginController implements Initializable{
+	
+	@FXML Button signup;
+	@FXML TextField username;
+	@FXML Button login;
+	@FXML ImageView progress;
+	@FXML TextField password;
+	
+	private Connection conn;
+	private String users;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		progress.setVisible(false);
+	}
+	public void loginAction(ActionEvent event) {
+		//로딩바 
+		progress.setVisible(true);
+		PauseTransition pt = new PauseTransition();
+		pt.setDuration(Duration.seconds(3));
+		pt.setOnFinished(event1 -> {
+			System.out.println("Login Succesfully");
+		});
+		
+		pt.play();
+	}
+	public void signup(ActionEvent evnet2) throws IOException {
+		Stage signup = new Stage();
+		login.getScene().getWindow().hide();
+		Parent root = FXMLLoader.load(getClass().getResource("/FXML/SignUp.fxml"));
+		Scene scene = new Scene(root);
+		signup.setScene(scene);
+		signup.show();
+		signup.setResizable(false);
+	}
 }
