124e3bee21269d1b294db53b9992b0b8720c281c
Yangsudong <59611108+Yangsudong@users.noreply.github.com>
2020-06-15 12:15:02 +0900

    Update reviewHomeController.java

--- src/javaYJProject/reviewHomeController.java
+++ src/javaYJProject/reviewHomeController.java
@@ -10,6 +10,22 @@ public class reviewHomeController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
+	public void logoutAction(ActionEvent ae1) {
+
+		btnLogout.getScene().getWindow().hide();
+
+		Stage login = new Stage();
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("login.fxml"));
+			Scene scene = new Scene(root);
+			login.setScene(scene);
+			login.show();
+			login.setResizable(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
